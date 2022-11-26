dbe4b0b80860bfa59569beeacde94b0ebc3f4a23
Amin Saedi <amin.di.sam@gmail.com>
2022-11-25 20:55:07 -0500

    basic bot module added

---
+++ bot/main.py
@@ -0,0 +1,22 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, filters, MessageHandler, ContextTypes
+
+from rj_converter import convert_link
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        link = convert_link(update.message.text)
+        await update.message.reply_audio(link)
+    except:
+        await update.message.reply_text("Error sending music")
+
+
+token = os.environ.get("BOT_TOKEN")
+if (token == None):
+    raise Exception("No token in BOT_TOKEN env")
+app = ApplicationBuilder().token(token).build()
+
+app.add_handler(MessageHandler(filters.TEXT, hello))
+
+app.run_polling()
